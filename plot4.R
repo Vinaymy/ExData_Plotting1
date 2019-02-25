2ecd77f89604423fee92d81e2e0f08139bfb2330
Vinay Yadappanavar <vinaymy@gmail.com>
2019-02-24 19:51:24 -0800

    Moved around par command so that png is created properly

--- plot4.R
+++ plot4.R
@@ -1,5 +1,3 @@
-par(mfrow=c(2,2))
-
 my_data <- read.csv("eda/ExData_Plotting1/household_power_consumption.txt", header = TRUE, sep = ";", dec = ".")
 my_data$Date <- as.Date((my_data$Date), format = "%d/%m/%Y")
 my_data <- my_data[my_data$Date >= "2007-02-01" & my_data$Date <= "2007-02-02",]
@@ -13,6 +11,7 @@ my_data$Sub_metering_2 <- as.numeric(as.character(my_data$Sub_metering_2))
 my_data$Sub_metering_3 <- as.numeric(as.character(my_data$Sub_metering_3))
 
 png('plot4.png')
+par(mfrow=c(2,2))
 plot(my_data$Global_active_power~my_data$DateTime, type = "l", ylab="Global Active Power", xlab="")  
 plot(my_data$Voltage~my_data$DateTime, type = "l", ylab="Voltage", xlab="")
 plot(my_data$Sub_metering_1~my_data$DateTime, type = "l", ylab="Energy sub metering", xlab="")
